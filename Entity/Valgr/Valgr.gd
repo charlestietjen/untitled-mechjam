2757f1d5941f3f38712787df4bc6a93e47ef1c67
Phil Tietjen <tietjen.philip@gmail.com>
2022-07-20 01:19:51 -0400

    valgr: simple IDLE and ALERT state logic

--- Entity/Valgr/Valgr.gd
+++ Entity/Valgr/Valgr.gd
@@ -1,32 +1,46 @@
 extends "res://script/Actor.gd"
 
+enum {
+	IDLE,
+	ALERT
+}
+
+var state = IDLE
+
 onready var nav = get_node("/root/Spatial/AStar")
 onready var player = get_node("/root/Spatial/Player")
+onready var raycasts = $RayCasts
+var velocity := Vector3(0,0,0)
 
 func _ready():
 	health = maxHealth
 	$AnimationTree.active = true
 
 func _physics_process(delta):
-	if current_velocity != Vector3.ZERO:
+	if velocity != Vector3.ZERO:
 		$AnimationTree.set("parameters/move_state/current", 1)
 	else:
 		$AnimationTree.set("parameters/move_state/current", 0)
-	if int(current_velocity.x) > 0:
+	if int(velocity.x) > 0:
 		rotation.y = lerp_angle(rotation.y, 90, 0.5)
-	elif int(current_velocity.x) < 0:
+	elif int(velocity.x) < 0:
 		rotation.y = lerp_angle(rotation.y, 180, 0.5)
-	var new_velocity := Vector3.ZERO
 	var lerp_weight = delta * 8.0
+
 	if current_target != Vector3.INF:
 		var dir_to_target = global_transform.origin.direction_to(current_target).normalized()
-		new_velocity = lerp(current_velocity, speed * dir_to_target, lerp_weight)
+		velocity = lerp(velocity, speed * dir_to_target, lerp_weight)
 		if global_transform.origin.distance_to(current_target) < 0.5:
 			find_next_point_in_path()
 	else:
-		new_velocity = lerp(current_velocity, Vector3.ZERO, lerp_weight)
+		velocity = lerp(velocity, Vector3.ZERO, lerp_weight)
 	global_transform.origin.z = lerp(global_transform.origin.z, 0, 0.1)
-	current_velocity = move_and_slide(new_velocity)
+	velocity = move_and_slide(velocity)
+	
+	for ray in raycasts.get_children():
+		if(ray.get_collider() == player):
+			state = ALERT
+			break
 
 func find_next_point_in_path():
 	if path.size() > 0:
@@ -48,4 +62,10 @@ func _on_Area_area_entered(area):
 
 
 func _on_Timer_timeout():
-	update_path(nav.find_path(global_transform.origin, player.global_transform.origin))
+	if(state == ALERT):
+		update_path(nav.find_path(global_transform.origin, player.global_transform.origin))
+
+
+func _on_IdleRotateTimer_timeout():
+	# on time out, stop, turn the fuck around, and move
+	pass
