4581d30fdd5cb5250eeaad021934cd4d6e994774
Charles <tietjen.charles@gmail.com>
2022-07-20 20:05:30 -0400

    UI stuff

--- UI/StartUI.gd
+++ UI/StartUI.gd
@@ -1,19 +1,42 @@
 extends Control
 
-
 func _ready():
-	$PressStart.grab_focus()
+	$MenuLayer/PressStart.grab_focus()
+	get_viewport().connect("gui_focus_changed", self, "_on_focus_changed")
+	$MenuLayer/controlsLayout/backButton.connect("button_up", self, "_on_controls_back_up")
 
 
 func _on_PressStart_button_up():
-	$PressStart.visible = not visible
-	$Menu.visible = visible
-	$Menu/Start.grab_focus()
+	$MenuLayer/PressStart.visible = not visible
+	$MenuLayer/Menu.visible = visible
+	$MenuLayer/Menu/Start.grab_focus()
 
 
 func _on_Start_button_up():
+	$BGLayer/AnimationPlayer.play("transition")
+	$buttonDown.play()
+	$MenuLayer/Menu.visible = not visible
+	yield(get_tree().create_timer(1.8), "timeout")
 	SceneChanger.goto_scene("res://UI/LoadingUI.tscn")
 
-
 func _on_Quit_button_up():
+	$buttonDown.play()
+	$MenuLayer/Menu.visible = not visible
+	yield(get_tree().create_timer(1.0), "timeout")
 	get_tree().quit()
+
+func _on_focus_changed(event):
+	$buttonFocus.play()
+
+
+func _on_Control_button_up():
+	$buttonDown.play()
+	$MenuLayer/Menu.visible = not visible
+	$MenuLayer/controlsLayout.visible = visible
+	$MenuLayer/controlsLayout/backButton.grab_focus()
+
+func _on_controls_back_up():
+	$buttonDown.play()
+	$MenuLayer/controlsLayout.visible = not visible
+	$MenuLayer/Menu.visible = visible
+	$MenuLayer/Menu/Control.grab_focus()
